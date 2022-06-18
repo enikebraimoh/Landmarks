f9f9ee8aeeb007e7bb12e6b729fbcc073d87e658
enikebraimoh <richardbraimoh@gmail.com>
2022-06-19 00:59:07 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Enike Braimoh on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
